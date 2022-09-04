dc26cde7098a5a2e9f34ed77ed22dae9085a7216
juli0mendez <75279400+juli0mendez@users.noreply.github.com>
2022-09-04 15:05:27 -0400

    Update HellowWolf.ps1

--- HellowWolf.ps1
+++ HellowWolf.ps1
@@ -1,2 +1,14 @@
- $Date = Get-Date
-"Hello Wolf we got in $Date" | Out-File -FilePath c:\temp\Persistence_Achieved.txt -Append 
+#Get Date for file name
+$FileName = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")
+
+#Create File with date
+New-Item -itemType File -Path c:\temp -Name ("Persistence_Achieved "+ $FileName + ".log")
+
+#Set file to write to
+$TargetFile = ("c:\temp\Persistence_Achieved "+ $FileName + ".log")
+
+#Get data to write in file
+$Date = Get-Date
+
+#Write msg into new file created
+"Hello Wolf we got in $Date" | Out-File -FilePath $TargetFile -Append 
